c6886e55aba2e50791025a3f1aaa7c403aca82e1
Said Kurt <mmsskk94@gmail.com>
2021-11-10 00:31:58 +0300

    Initial Commit

---
+++ basic_calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  basic_calculator
+//
+//  Created by Said Kurt on 10.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
