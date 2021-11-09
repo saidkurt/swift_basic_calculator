90146db1247f3baf674eddd0153437e51e3efad1
Said Kurt <mmsskk94@gmail.com>
2021-11-10 01:30:17 +0300

    try

--- basic_calculator/ViewController.swift
+++ basic_calculator/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstValue: UITextField!
+    @IBOutlet weak var secondValue: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var sonuc: UILabel!
+    @IBAction func buttonBol(_ sender: Any) {
+        sonuc.text="Sonuç :" + String(Int(firstValue.text!)! / Int(secondValue.text!)!)
+    }
+    @IBAction func buttonCarp(_ sender: Any) {
+        sonuc.text="Sonuç :" + String(Int(firstValue.text!)! * Int(secondValue.text!)!)
+    }
+    @IBAction func buttonCikar(_ sender: Any) {
+        sonuc.text="Sonuç :" + String(Int(firstValue.text!)! - Int(secondValue.text!)!)
+    }
+    
+    @IBAction func buttonTopla(_ sender: Any) {
+        sonuc.text="Sonuç :" + String(Int(firstValue.text!)! + Int(secondValue.text!)!)
+    }
 }
 
